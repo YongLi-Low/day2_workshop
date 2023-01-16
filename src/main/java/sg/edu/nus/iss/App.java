a8792249714cb4e7147883151e9d719b55a7b530
Yongli <yl.low91@gmail.com>
2023-01-16 12:37:02 +0800

    added FixedDepositAccount

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,8 @@
 package sg.edu.nus.iss;
 
+import java.util.Random;
+import java.util.UUID;
+
 /**
  * Hello world!
  */
@@ -9,9 +12,47 @@ public final class App {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Banking");
+
+        int n = 3; // number of bank accounts the array can store
+        BankAccount bankAccount[] = new BankAccount[n];
+
+        for (int i = 0; i < bankAccount.length; i++) {
+            String uuid = UUID.randomUUID().toString();
+
+            double max = 10_000;
+            double min = 1_000;
+
+            Random randomNum = new Random();
+            double initialBalance = min; // + randomNum.nextDouble(max * 0.1);
+
+            bankAccount[i] = new FixedDepositAccount(uuid, initialBalance);
+        }
+
+        bankAccount[0].setFullName("Tan Ah Beng");
+        bankAccount[1].setFullName("Lee Ah Seng");
+        bankAccount[2].setFullName("Lim Ah Heng");
+
+        for (int j = 0; j < bankAccount.length; j++) {
+            bankAccount[j].showAccount();
+        }
+
+        bankAccount[0].deposit(2000);
+        bankAccount[1].deposit(4000);
+        bankAccount[2].deposit(5000);
+
+        for (int j = 0; j < bankAccount.length; j++) {
+            bankAccount[j].showAccount();
+        }
+
+        bankAccount[0].withdraw(2000);
+        bankAccount[1].withdraw(4000);
+        bankAccount[2].withdraw(10000);
+
+
     }
 }
--- src/main/java/sg/edu/nus/iss/BankAccount.java
+++ src/main/java/sg/edu/nus/iss/BankAccount.java
@@ -26,7 +26,7 @@ public class BankAccount {
 
     public Date getAccountEndDate() { return accountEndDate; }
     public void setAccountEndDate(Date accountEndDate) { this.accountEndDate = accountEndDate; }
-    
+
     public BankAccount(String fullName, String accountNo, double balance, boolean isActive, Date accountStartDate,
             Date accountEndDate) {
         this.fullName = fullName;
@@ -37,5 +37,55 @@ public class BankAccount {
         this.accountEndDate = accountEndDate;
     }
 
-    
+    public BankAccount(String fullName, String accountNo, double balance) {
+        this.fullName = fullName;
+        this.accountNo = accountNo;
+        this.balance = balance;
+    }
+
+    public BankAccount(String accountNo, double balance) {
+        this.accountNo = accountNo;
+        this.balance = balance;
+    }
+
+    public void showAccount() {
+        System.out.println(("Account No: " + accountNo));
+        System.out.println(("Fullname: " + fullName));
+        System.out.println("Balance: " + balance);;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount [fullName=" + fullName + ", accountNo=" + accountNo + ", balance=" + balance + ", isActive="
+                + isActive + "]";
+    }
+
+    public void deposit(double amount) {
+
+        if (!isActive) {
+            throw new IllegalArgumentException("You cannot make deposit to a closed account!");
+        }
+
+        if (amount < 0) {
+            throw new IllegalArgumentException("You cannot make negative deposit!");
+        }
+        else {
+            balance += amount;
+        }
+
+    }
+
+    public void withdraw(double amount) {
+
+        if (!isActive) {
+            throw new IllegalArgumentException("You cannot make withdrawal out a of closed account!");
+        }
+
+        if (balance < amount) {
+            throw new IllegalArgumentException("Your balance is less than " + amount);
+        }
+        else {
+            balance -= amount;
+        }
+    }
 }
